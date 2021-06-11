2aae96ff001356eab192fd9cb5d43b5a30975f9d
Simon <sera.-@hotmail.com>
2021-06-10 22:08:34 -0500

    camibos

--- testOptions.py
+++ testOptions.py
@@ -1,5 +1,61 @@
 import sys, getopt
 from cryptography.fernet import Fernet
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+
+
+def decifraElArchivo(archivo):
+    print('Decifra con RSA: ', archivo)
+    file_in = open(archivo, "wb+")
+
+    private_key = RSA.import_key(open("private.pem").read())
+
+    enc_session_key, nonce, tag, ciphertext = [ file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+
+    # Decrypt the session key with the private RSA key
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    session_key = cipher_rsa.decrypt(enc_session_key)
+
+    # Decrypt the data with the AES session key
+    cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+    data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+    #file_in.write(data)
+    #file_in.close()
+    print(data.decode("utf-8"))
+    return
+
+def cifraElArchivo(archivo):
+    print('Cifrado con RSA: ', archivo)
+
+    generaClaves()
+    file_out = open(archivo, "wb+")
+
+    recipient_key = RSA.import_key(open("receiver.pem").read())
+    session_key = get_random_bytes(16)
+
+    # Encrypt the session key with the public RSA key
+    cipher_rsa = PKCS1_OAEP.new(recipient_key)
+    enc_session_key = cipher_rsa.encrypt(session_key)
+
+    # Encrypt the data with the AES session key
+    cipher_aes = AES.new(session_key, AES.MODE_EAX)
+    ciphertext, tag = cipher_aes.encrypt_and_digest(file_out.read())
+    [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
+    file_out.close()
+
+
+def generaClaves():
+    key = RSA.generate(2048)
+    private_key = key.export_key()
+    file_out = open("private.pem", "wb")
+    file_out.write(private_key)
+    file_out.close()
+
+    public_key = key.publickey().export_key()
+    file_out = open("receiver.pem", "wb")
+    file_out.write(public_key)
+    file_out.close()
 
 def imprimeAyuda():
    print("""
@@ -23,14 +79,9 @@ def elArchivoEsValido(archivo):
     except IOError as e:
         return False
 
-
-def cargarClave(clave="clave.key"):
-    #abre el archivo de la llave
-    return open(clave, "wb").read
-
 def generaClave(clave="clave.key"):
     claveCifrado = Fernet.generate_key()
-    with open( clave, "wb+") as archivoClave:
+    with open( clave, "wb") as archivoClave:
         archivoClave.write(claveCifrado)
     return clave
 
@@ -38,6 +89,7 @@ def cifraElArchivoSimetrico(archivo):
     print('Cifrado simetrico de ', archivo)
     with open(generaClave(), "rb") as key:
         ferni = Fernet(key.read())
+        key.close()
     with open(archivo, "wb+") as file:
         datosCifrados = ferni.encrypt(file.read())
         file.write(datosCifrados)
@@ -46,12 +98,11 @@ def cifraElArchivoSimetrico(archivo):
 
 def decifraElArchivoSimetrico(archivo):
     print('Decifrado simetrico de ', archivo)
-    #key = "clave.key"
-    key = input("Ingresa el nombre de la clave: ")
+    key = "clave.key"
+    #key = input("Ingresa el nombre de la clave: ")
     try:
         with open(key, 'rb') as f:
             ferni = Fernet(f.read())
-            
             with open(archivo, "rb") as file:
                 encrypted_data = file.read()
                 decrypted_data = ferni.decrypt(encrypted_data)
@@ -61,6 +112,7 @@ def decifraElArchivoSimetrico(archivo):
                 file.write(decrypted_data)
                 print(decrypted_data)
                 file.close()
+            f.close()
             
 
     except FileNotFoundError as e:
@@ -69,16 +121,6 @@ def decifraElArchivoSimetrico(archivo):
     except IOError as e:
         print("El archivo de la llave es invalido")
 
-
-
-
-def cifraElArchivo(archivo):
-    print('Aqui se inserta RSA')
-    return
-
-def decifraElArchivo(archivo):
-    print('Aqui se inserta AES')
-    return
 
 def main(argv):
    inputfile = ''
